a49c4d46383f879340582fc525513b1af686234a
Perla Ordonez <guate.po@gmail.com>
2019-04-17 13:41:10 -0500

    adding src files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () =>(
+    <footer className="footer mt-auto py-3">
+        <div className ="container">
+            <span className="text-muted">Clicky Game!</span>
+        </div>
+    </footer>
+);
+
+export default Footer;

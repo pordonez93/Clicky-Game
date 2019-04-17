19e7b9970ac76c41ee96e583a37f4fe103c22125
Perla Ordonez <guate.po@gmail.com>
2019-04-17 14:04:56 -0500

    adding in files for images and functionality

---
+++ src/images/index.js
@@ -0,0 +1,76 @@
+import img1 from "./arrowhead-stadium.jpg"
+import img2 from "./kauffman-stadium.jpg"
+import img3 from "./kc-library.jpg"
+import img4 from "./beer-hall.jpg"
+import img5 from "./sprint-center.jpg"
+import img6 from "./starlight-theater.jpg"
+import img7 from "./the-blue-room.jpg"
+import img8 from "./the-midland.jpg"
+import img9 from "./the-nelson.jpg"
+import img10 from "./the-scout.jpg"
+import img11 from "./union-station.jpg"
+import img12 from "./WWI-museum.jpg"
+
+export default [
+    {
+      "id": 1,
+      "image": img1,
+      "clicked": false
+    },
+    {
+      "id": 2,
+      "image": img2,
+      "clicked": false
+    },
+    {
+      "id": 3,
+      "image": img3,
+      "clicked": false
+    },
+    {
+      "id": 4,
+      "image": img4,
+      "clicked": false
+    },
+    {
+      "id": 5,
+      "image": img5,
+      "clicked": false
+    },
+    {
+      "id": 6,
+      "image": img6,
+      "clicked": false
+    },
+    {
+      "id": 7,
+      "image": img7,
+      "clicked": false
+    },
+    {
+      "id": 8,
+      "image": img8,
+      "clicked": false
+    },
+    {
+      "id": 9,
+      "image": img9,
+      "clicked": false
+    },
+    {
+      "id": 10,
+      "image": img10,
+      "clicked": false
+    },
+    {
+      "id": 11,
+      "image": img11,
+      "clicked": false
+    },
+    {
+      "id": 12,
+      "image": img12,
+      "clicked": false
+    }
+  ]
+
